3f3740bef8e047732ccc5c55f8d7aebb25090edc
joacosan <joacosanchezpereira@hotmail.com>
2021-05-23 10:49:39 -0300

    r4 finalizado, se uso una api de heroku por problemas con la dada

--- r4/main.js
+++ r4/main.js
@@ -1,26 +1,49 @@
 document.addEventListener('DOMContentLoaded',()=>{
-let btnCargar = document.querySelector('.btn')
-let aviso = document.querySelector('.aviso')
-let table  = document.querySelector('.table')
+let btnCargar = document.querySelector('.btn');
+let aviso = document.querySelector('.aviso');
+let url = "http://web-unicen.herokuapp.com/api/groups/grupo33/Menu/";
 btnCargar.addEventListener('click',async()=>{
     aviso.innerHTML = "cargando..."
-    try{
-        let url = "https://jsonplaceholder.typicode.com/users";
-        let r = await fetch(url)
-        let json = r.json();
-        let html = "";
-
-
-            let tBody = document.querySelector(".table");
+    try {
+        aviso.innerHTML = "Cargando tabla..";
+        let r = await fetch(url);
+        let json = await r.json();
+        if (json.Menu.length > 0) {
+            let tBody = document.getElementById("table-body");
             tBody.innerHTML = "";
             console.log("skere");
-            for (let i = 0; i < json.users.length; i++) {
-                insertarTablaJson(json.users[i]);
+            for (let i = 0; i < json.Menu.length; i++) {
+                insertarTablaJson(json.Menu[i]);
             }
             aviso.innerHTML = "Se cargo con exito!";
-    }catch(e){
-        aviso.innerHTML = "ERORR"
+        } else {
+            aviso.innerHTML = "La Tabla esta vacia";
+        }
+    } catch (e) {
+        aviso.innerHTML = "Fallo el cargado de la tabla";
     }
-    
-})
-})
+});
+    function insertarTablaJson(menu) {
+        let tBody = document.getElementById("table-body");
+        let tRow = tBody.insertRow(0);
+        let tCell1 = tRow.insertCell(0),
+        tCell2 = tRow.insertCell(1),
+        tCell3 = tRow.insertCell(2),
+        tCell4 = tRow.insertCell(3),
+        tCell5 = tRow.insertCell(4);
+
+        tRow.id = menu._id;
+
+        tCell1.id = "celdaEvent";
+        tCell2.id = "celdaEvent";
+        tCell3.id = "celdaEvent";
+        tCell4.id = "celdaEvent";
+        tCell5.id = "celdaEvent";
+
+        tCell1.innerHTML = menu.thing.cantidadCarnes;
+        tCell2.innerHTML = menu.thing.quesoElegido;
+        tCell3.innerHTML = menu.thing.verduraElegida;
+        tCell4.innerHTML = menu.thing.aderezoElegido;
+        tCell5.innerHTML = menu.thing.mailCreador;
+    }
+});
